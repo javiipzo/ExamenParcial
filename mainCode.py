4719f738eee4400aec87b8acedf700cffe3b0a4c
javiipzo <javipozo999@gmail.com>
2021-11-18 17:01:35 +0100

    Otroerror

--- mainCode.py
+++ mainCode.py
@@ -1,13 +1,16 @@
 import pandas as pd
+from pandas.core.frame import DataFrame
 
 def leerdatos(archivo ):
     df=pd.read_csv(archivo)
-    print(df)
+    df
+
+    
 
 def separarColumnas(archivo):
     df=pd.read_csv(archivo)
     df.shape
     lista = []
     for i in range(df.shape[0]):
-        valores = df._get_value(i, "camp;adg;sl;adv")
+        valores = df.get_value(i, "camp;adg;sl;adv")
         lista.append(valores.split(";"))
