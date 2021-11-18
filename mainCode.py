1455a9ebcbe80f4bdc9259af9fee02819821165a
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-18 18:28:40 +0100

    no salía na:)

--- mainCode.py
+++ mainCode.py
@@ -9,22 +9,4 @@ def getvalue(archivo,fila,dato):
     df=pd.read_csv(archivo)
     df._get_value(fila,dato)
 
-def separarColumnas(archivo):
-    df=pd.read_csv(archivo)
-    df.shape
-    lista = []
-    for i in range(df.shape[0]):
-        valores = df._get_value(i, "url_landing")
-        lista.append(valores.split(";"))
 
-def no_id(archivo):
-    df=pd.read_csv(archivo)
-    df.shape
-    lista = []
-    for i in range(df.shape[0]):
-        valores= df._get_value(i, "id_user")
-        if "id_user" == None:
-            valores= df._get_value(i, "gclid")
-        elif "gclid" == None:
-            valores= df._get_value(i, "user_recurrent")
-        lista.append(valores.split(";"))
