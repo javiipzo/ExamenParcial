0674642c44d59e8e52de63d52b71dccddb315fc6
eherraiz <eherraiz@myuax.com>
2021-11-18 18:49:29 +0100

    aica

--- mainCode.py
+++ mainCode.py
@@ -2,11 +2,11 @@ import pandas as pd
 from pandas.core.frame import DataFrame
 import urllib
 def leerdatos(archivo ):
-    df=pd.read_csv(archivo)
-    print(df)
+    df = pd.read_csv(archivo)
+    return df
 
 def getvalue(archivo,fila,dato):
-    df=pd.read_csv(archivo)
+    df = pd.read_csv(archivo)
     df._get_value(fila,dato)
 
 def separarColumnas(archivo):
