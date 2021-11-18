a88a13f75439d15d0a160aee2f9d61d5237a6fcd
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-18 17:42:56 +0100

    def no id

--- mainCode.py
+++ mainCode.py
@@ -16,3 +16,15 @@ def separarColumnas(archivo):
     for i in range(df.shape[0]):
         valores = df._get_value(i, "url_landing")
         lista.append(valores.split(";"))
+
+def no_id(archivo):
+    df=pd.read_csv(archivo)
+    df.shape
+    lista = []
+    for i in range(df.shape[0]):
+        valores= df._get_value(i, "id_user")
+        if "id_user" == None:
+            valores= df._get_value(i, "gclid")
+        elif "gclid" == None:
+            valores= df._get_value(i, "user_recurrent")
+        lista.append(valores.split(";"))
