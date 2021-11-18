e05fe407af7585034611d37aa7b058d4934dade3
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-18 16:34:26 +0100

    def separar

--- mainCode.py
+++ mainCode.py
@@ -1,14 +1,9 @@
 import pandas as pd
 
-<<<<<<< HEAD
-def leerdatos(archivo):
-    df=pd.read_csv(archivo)
-    print(df)
-
-leerdatos("navegacion.csv")
-=======
 def leerdatos(archivo ):
     df=pd.read_csv(archivo)
+    
 
-
->>>>>>> 830d935c15c2c484180fcbd7316762b69154102c
+def separarColumnas(archivo):
+    df=pd.read_csv(archivo)
+    df.shape
