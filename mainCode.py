2077c9c2d3b3c9e15ba81ffe01a24382272902eb
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-18 17:03:16 +0100

    vuelvo a un bucle solo

--- mainCode.py
+++ mainCode.py
@@ -3,16 +3,10 @@ import pandas as pd
 def leerdatos(archivo ):
     df=pd.read_csv(archivo)
     print(df)
-def separarColumnas():
-    df1=pd.read_csv("conversiones.csv")
-    df1.shape
-    listaConversiones = []
-    for i in range(df1.shape[0]):
-        valores = df1._get_value(i, "camp;adg;sl;adv")
-        listaConversiones.append(valores.split(";"))
-    df2=pd.read_csv("navegacion.csv")
-    df2.shape
-    listaNavegación = []
-    for i in range(df2.shape[0]):
-        valores = df2._get_value(i, "camp;adg;sl;adv")
-        listaNavegación.append(valores.split(";"))
+def separarColumnas(archivo):
+    df=pd.read_csv(archivo)
+    df.shape
+    lista = []
+    for i in range(df.shape[0]):
+        valores = df._get_value(i, "camp;adg;sl;adv")
+        lista.append(valores.split(";"))
