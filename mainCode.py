9bc525d5b50f65b8cb2b323b8dbf08be8d1a3d17
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-18 16:41:06 +0100

    metodo separar a mejorar

--- mainCode.py
+++ mainCode.py
@@ -8,3 +8,7 @@ def leerdatos(archivo ):
 def separarColumnas(archivo):
     df=pd.read_csv(archivo)
     df.shape
+    lista = []
+    for i in range(df.shape[0]):
+        valores = df._get_value(i, "date;hour;id_lead;id_user;gclid;lead_type;result")
+        lista.append(valores.split(";"))
