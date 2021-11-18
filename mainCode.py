79db9fe2176ecdda9459bb82cfb73a9282d972d2
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-18 16:42:09 +0100

    creo que ya

--- mainCode.py
+++ mainCode.py
@@ -10,5 +10,5 @@ def separarColumnas(archivo):
     df.shape
     lista = []
     for i in range(df.shape[0]):
-        valores = df._get_value(i, "date;hour;id_lead;id_user;gclid;lead_type;result")
+        valores = df._get_value(i, "camp;adg;sl;adv")
         lista.append(valores.split(";"))
