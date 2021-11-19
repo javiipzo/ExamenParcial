2071c32b5ae38f4f231fcce949ad2a00773adb77
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-19 14:20:45 +0100

    segundo ontento

--- ej3punto2.py
+++ ej3punto2.py
@@ -10,10 +10,10 @@ print(navegacion.head())
 Df = navegacion['url_landing'].str.split('&', expand = True)
 print(Df.head())
 Df.columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec', 'vacía']
-if navegacion['id_user'] == None:
-    navegacion['gclid']
-    if navegacion['gclid'] == None:
-        navegacion['user_recurrent']
+if Df['id_user'] == None:
+    Df['gclid']
+    if Df['gclid'] == None:
+        Df['user_recurrent']
 
 
 
