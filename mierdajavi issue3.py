db5d9fde55ab678fd718e29b5f570fb18ed60d03
javiipzo <javipozo999@gmail.com>
2021-11-19 17:23:07 +0100

    cosasmias

--- mierdajavi issue3.py
+++ mierdajavi issue3.py
@@ -12,11 +12,12 @@ def usuariosRepes(archivo):
     df=pd.read_csv(archivo,header=0, sep=";")
     navegacion = leerdatos(archivo)
 
-    print(navegacion.head())
     Df = navegacion['url_landing'].str.split('&', expand = True)
-    print(Df.head())
     Df.columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec',' ']
     Ddf=Df.drop(['url_landing','uuid','camp','adg','device','sl','adv','rec'], axis=1)
     print(Ddf.head())
+    print("\n")
+
 
 usuariosRepes("navegacion.csv")
+
