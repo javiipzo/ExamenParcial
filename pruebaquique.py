2547783d4ae878d42ac3ec76ee65b6b525c34ec8
eherraiz <eherraiz@myuax.com>
2021-11-19 15:41:57 +0100

    wjei

--- pruebaquique.py
+++ pruebaquique.py
@@ -13,7 +13,11 @@ navegacion = leerdatos('navegacion.csv')
 conversion = leerdatos('conversiones.csv')
 print(navegacion.head())
 Df = navegacion['url_landing'].str.split('&', expand = True)
-print(Df.head())
 Df.columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec', 'vacía']
-Df.drop(['vacía'], axis=1)
+Df.drop(['vacía'], axis=1) #1 es columnas para las columnas
 print(Df.head())
+new_df = Df['url_landing'].str.split('/', 4, expand = True)
+new_df = new_df.drop(new_df.columns[[0, 1, 2, 3]], axis = 'columns')
+print(new_df.head())
+modelo = new_df[new_df.columns[0]].str.split('/', expand = True)
+print(modelo.head())
