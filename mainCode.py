d2b331dd23b3cc914c40f03cce47a4048acf44c0
eherraiz <eherraiz@myuax.com>
2021-11-19 12:04:04 +0100

    j

--- mainCode.py
+++ mainCode.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pandas.core.frame import DataFrame
-import urllib.parse
+import urllib.parse 
 #import urllib3
 #para leer el fichero
 def leerdatos(archivo ):
@@ -29,10 +29,11 @@ obtenerPartesURL("navegacion.csv",5)
 navegacion = separarColumnas('navegacion.csv')
 conversion = separarColumnas('conversiones.csv')
 Df = pd.DataFrame(navegacion.loc[:, 'url_landing'])#, columns = ['camp', 'adg', 'device', 'adv', 'sl'])
-Df.to_csv('url_landing.csv') 
+Df.to_csv('url_landing.csv')
 print(Df)
 
 new_df = pd.read_csv('url_landing.csv')
 new_df = pd.loc[:, 'url_landing']
 
 new_df = pd.DataFrame(new_df, columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec'])
+pui
