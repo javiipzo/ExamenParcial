c4b53b6ef44ce6e4ae8a9690b8d3e7a408461af4
javiipzo <javipozo999@gmail.com>
2021-11-19 12:07:02 +0100

    cambio actu

--- mainCode.py
+++ mainCode.py
@@ -6,34 +6,20 @@ import urllib.parse
 def leerdatos(archivo ):
     df = pd.read_csv(archivo)
     return df
-#buscar un dato en concreto de un fichero a partir de la fila y columna
-def getvalue(archivo,fila,dato):
-    df = pd.read_csv(archivo)
-    df._get_value(fila,dato)
 
 #Separar todo en columnas
 def separarColumnas(archivo):
     df=pd.read_csv(archivo, sep=";")
 
 #obtener todas las partes de la url, issue2
-def obtenerPartesURL(archivo,columna):
-    df=pd.read_csv(archivo, sep=";")
-    #primero obtengo la URL
-    df.iloc[columna]
-    partes = []
-    partes.append (df.urllib.parse)
-    print(partes)
+def obtenerPartesURL(archivo):
+    navegacion = separarColumnas('navegacion.csv')
+    Df = pd.DataFrame(navegacion.loc[:, 'url_landing'])#, columns = ['camp', 'adg', 'device', 'adv', 'sl'])
+    Df.to_csv('url_landing.csv')
+    print(Df)
 
-obtenerPartesURL("navegacion.csv",5)
+    new_df = pd.read_csv('url_landing.csv')
+    new_df = pd.loc[:, 'url_landing']
 
-navegacion = separarColumnas('navegacion.csv')
-conversion = separarColumnas('conversiones.csv')
-Df = pd.DataFrame(navegacion.loc[:, 'url_landing'])#, columns = ['camp', 'adg', 'device', 'adv', 'sl'])
-Df.to_csv('url_landing.csv')
-print(Df)
-
-new_df = pd.read_csv('url_landing.csv')
-new_df = pd.loc[:, 'url_landing']
-
-new_df = pd.DataFrame(new_df, columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec'])
+    new_df = pd.DataFrame(new_df, columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec'])
 
