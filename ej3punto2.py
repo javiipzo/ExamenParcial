a33760fa78f2e60bf3329840bad3a3142ea4bd81
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-19 14:34:25 +0100

    intento3

--- ej3punto2.py
+++ ej3punto2.py
@@ -6,15 +6,20 @@ def mostrar_datos(archivo):
     print(archivo)
 navegacion = leerdatos('navegacion.csv')
 conversion = leerdatos('conversiones.csv')
-print(navegacion.head())
+#print(navegacion.head())
 Df = navegacion['url_landing'].str.split('&', expand = True)
-print(Df.head())
+#print(Df.head())
 Df.columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec', 'vacía']
-if Df['id_user'] == None:
-    Df['gclid']
-    if Df['gclid'] == None:
-        Df['user_recurrent']
-
+id=Df.shape['id_user']
+gclid = Df.shape['gclid']
+user_recurrent = Df.shape['user_recurrent']
+for i in range (id):
+    if id == None:
+        gclid
+        for i in range (gclid):
+            if gclid == None:
+                user_recurrent
+print(i)
 
 
 
