e5331f656f657ad0b88bfc0aa45f6f1ee84d6341
eherraiz <eherraiz@myuax.com>
2021-11-19 12:56:26 +0100

    43

--- pruebaquique.py
+++ pruebaquique.py
@@ -14,5 +14,6 @@ conversion = leerdatos('conversiones.csv')
 print(navegacion.head())
 Df = navegacion['url_landing'].str.split('&', expand = True)
 print(Df.head())
-new_df = pd.DataFrame(Df, columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec'])
-print(new_df)
+Df.columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec', 'vacía']
+Df.drop(['vacía'], axis=1)
+print(Df.head())
