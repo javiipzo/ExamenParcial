91f9ab040e23aa99b93e399528775e987b88272b
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-19 09:39:44 +0100

    partes URL 1

--- mainCode.py
+++ mainCode.py
@@ -1,13 +1,23 @@
+import urllib
 import pandas as pd
 from pandas.core.frame import DataFrame
-import urllib
+from urllib import parse
+
 def leerdatos(archivo ):
-    df = pd.read_csv(archivo)
-    return df
+    df=pd.read_csv(archivo)
+    print(df)
 
 def getvalue(archivo,fila,dato):
-    df = pd.read_csv(archivo)
+    df=pd.read_csv(archivo)
     df._get_value(fila,dato)
 
 def separarColumnas(archivo):
     df=pd.read_csv("archivo", sep=";")
+leerdatos("navegacion.csv")
+
+def obtenerPartesURL(archivo):
+    df=pd.read_csv(archivo)
+    #primero obtengo la URL
+    DataFrame.iloc["url_landing"]
+    partes = urllib.parse.split("&")
+    for parte in partes:
