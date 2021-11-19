178a7a1fe3920225403c3cb1c3716a5cad15b5fa
javiipzo <javipozo999@gmail.com>
2021-11-19 10:41:11 +0100

    error

--- mainCode.py
+++ mainCode.py
@@ -2,22 +2,24 @@ import pandas as pd
 from pandas.core.frame import DataFrame
 import urllib
 import urllib3
+#para leer el fichero
 def leerdatos(archivo ):
     df = pd.read_csv(archivo)
     return df
-
+#buscar un dato en concreto de un fichero a partir de la fila y columna
 def getvalue(archivo,fila,dato):
     df = pd.read_csv(archivo)
     df._get_value(fila,dato)
 
+#Separar todo en columnas
 def separarColumnas(archivo):
-    df=pd.read_csv("archivo", sep=";")
-leerdatos("navegacion.csv")
+    df=pd.read_csv(archivo, sep=";")
 
+#obbtener todas las partes de la url, issue2
 def obtenerPartesURL(archivo,columna):
-    df=pd.read_csv(archivo)
+    df=pd.read_csv(archivo, sep=";")
     #primero obtengo la URL
-    DataFrame.iloc[columna]
+    df.iloc[columna]
     partes = urllib.parse.split("&")
 
-obtenerPartesURL("navegacion.csv","url_landing")
+obtenerPartesURL("navegacion.csv","Column 6")
