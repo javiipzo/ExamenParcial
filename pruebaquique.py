92af6d30935a08823da4537be7fc47b695bda7c9
eherraiz <eherraiz@myuax.com>
2021-11-20 19:42:21 +0100

    La ultima parte no le hagais ni puto caso

--- pruebaquique.py
+++ pruebaquique.py
@@ -7,53 +7,39 @@ import urllib
 def leerdatos(archivo):
     return pd.read_csv(archivo, sep = ';')
 
-def mostrar_datos(archivo):
-    print(archivo)
-    #para separar toda la url
-def separar_url(archivo):
-    navegacion = leerdatos(archivo)
-    print(navegacion.head())
-    Df = navegacion['url_landing'].str.split('&', expand = True)
-    Df.columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec', 'vacía']
-    Df.drop(['vacía'], axis=1) #1 es columnas para las columnas
-    print(Df.head())
-    new_df = Df['url_landing'].str.split('/', 4, expand = True)
-    new_df = new_df.drop(new_df.columns[[0, 1, 2, 3]], axis = 'columns')
-    print(new_df.head())
-    modelo = new_df[new_df.columns[0]].str.split('/', expand = True)
-    print(modelo.head())
-separar_url("navegacion.csv")
-    #para ver unicamente los idUser
-def usuariosRepes(archivo):
-    df=pd.read_csv(archivo,header=0, sep=";")
-    navegacion = leerdatos(archivo)
-
-    Df = navegacion['url_landing'].str.split('&', expand = True)
-    Df.columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec',' ']
-    Ddf=Df.drop(['url_landing','uuid','camp','adg','device','sl','adv','rec'], axis=1)
-    print(Ddf.head())
-    print("\n")
-
-<<<<<<< HEAD
+# Cargamos los datos en una variable
 navegacion = leerdatos('navegacion.csv')
 conversion = leerdatos('conversiones.csv')
-print(len(navegacion))
-Df = navegacion['url_landing'].str.split('&', expand = True)
-print(len(Df))
-Df.columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec', 'vacía']
-Df.drop(['vacía'], axis=1) #1 es columnas para las columnas
-new_df = Df['url_landing'].str.split('/', 4, expand = True)
-new_df = new_df.drop(new_df.columns[[0, 1, 2, 3]], axis = 'columns')
-modelo = new_df[new_df.columns[0]].str.split('/', expand = True)
-modelo = modelo.drop(modelo.columns[2], axis = 1)
-print(str.__contains__(modelo.loc[0, modelo.columns[0]], 'gclid'))
+print(navegacion.tail())
+
+# Obtenemos el resto de columnas a partir de la URL
+Datos_de_URL = navegacion['url_landing'].str.split('&', expand = True)
+Datos_de_URL.columns = ['url_landing','idUser', 'uuid', 'camp', 'adg', 'device', 'sl', 'adv', 'rec', 'vacía']
+
+# Nos faltaría sacar el modelo de coche de la URL
+modelo = Datos_de_URL['url_landing'].str.split('/', 4, expand = True)
+modelo = modelo.drop(modelo.columns[[0, 1, 2, 3]], axis = 'columns')
+modelo = modelo[modelo.columns[0]].str.split('/', expand = True)
+modelo = modelo.drop(modelo.columns[2], axis = 1) # axis = 1 para referirse a las columnas
+
+# Como hay URLs que no tienen modelo tenemos que cuadrar el dataframe
+modelo = modelo.astype(str)
+
+for i in range(0, len(modelo)):
+    if str.__contains__(modelo.loc[i, modelo.columns[0]], 'gclid'):
+        modelo.loc[i, modelo.columns[1]] = modelo.loc[i, modelo.columns[0]]
+        modelo.loc[i, modelo.columns[0]] = NaN
+    elif str.__contains__(modelo.loc[i, modelo.columns[0]], 'nan'):
+        modelo.loc[i, modelo.columns[0]] = NaN
+        modelo.loc[i, modelo.columns[1]] = NaN
+
+modelo.columns = ['Modelo', 'gclid']
+print(modelo.head())
+print(modelo.tail())
 print(len(modelo))
 
-# for i in range(0, len(modelo)):
-#     if str.__contains__(modelo.loc[i, modelo.columns[0]], 'gclid'):
-#         modelo.loc[i, modelo.columns[1]] = modelo.loc[i, modelo.columns[0]]
-#         modelo.loc[i, modelo.columns[0]] = NaN
-
-# print(modelo.head())
-=======
->>>>>>> bbe230c55f83235ce547a77f28420aa0666757ee
+# Eliminamos la columna URL_landing modificados
+Datos_de_URL = Datos_de_URL.drop(['url_landing', 'vacía'], axis = 1)
+frames = [navegacion, Datos_de_URL, modelo]
+Navegacion_final = pd.concat(frames)
+print(Navegacion_final.head())
